578ded959e9f8173d38b91a446d9cc99a6f2c511
James Blair <james.m.blair09@gmail.com>
2020-01-29 02:37:03 -0800

    Update entrypoint.R to account for NULL values in the request object

--- inst/plumber/pkg-api/entrypoint.R
+++ inst/plumber/pkg-api/entrypoint.R
@@ -12,6 +12,7 @@ if (!fs::dir_exists(config$log_dir)) fs::dir_create(config$log_dir)
 log_appender(appender_tee(tempfile("plumber_", config$log_dir, ".log")))
 
 convert_empty <- function(string) {
+  if (is.null(string)) return ("-")
   if (string == "") {
     "-"
   } else {
@@ -30,7 +31,7 @@ pr$registerHooks(
     postroute = function(req, res) {
       end <- tictoc::toc(quiet = TRUE)
       # Log details about the request and the response
-      log_info('{convert_empty(req$REMOTE_ADDR)} "{convert_empty(req$HTTP_USER_AGENT)}" {convert_empty(req$HTTP_HOST)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(res$status)} {round(end$toc - end$tic, digits = getOption("digits", 5))}')
+      log_info('{convert_empty(req$REMOTE_ADDR)} "{convert_empty(req$HTTP_USER_AGENT)}" {convert_empty(req$HTTP_HOST)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(res$status)} {round(end$toc - end$tic, digits = getOption("digits", 5))} {convert_empty(req$CONTENT_LENGTH)}')
     }
   )
 )
